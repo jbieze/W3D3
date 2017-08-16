f1e2e38b44a85e323a9b95c94d42be3a16c0adbb
App Academy Student <student@appacademy.io>
2017-08-16 14:54:05 -0700

    Complete URLShortener Phase I

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+
+  validates :email, presence: true, uniqueness: true
+
+end
